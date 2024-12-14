4715f8268bf80bbe2e7f93c554bcc2299d7ddf5b
SagirGarba <sagir40me@gmail.com>
2024-12-14 15:28:43 +0100

    Refactor: Organize Imorts Statements

---
+++ app/components/index.tsx
@@ -0,0 +1,5 @@
+export { default as ErrorMessage } from "./ErrorMessage";
+export { default as IssueStatusBar } from "./IssueStatusBar";
+export { default as Link } from "./Link";
+export { default as Spinner } from "./Spinner";
+export { default as Skeleton } from "./Skeleton";
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,6 +1,7 @@
+
+import { Skeleton } from "@/app/components";
 import { Box } from "@radix-ui/themes";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+
 
 const loadingNewIssuePAge = () => {
   return (
